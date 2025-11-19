a4f010e0cdfa16422dc8364acdf696976d786c3e
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2025-11-19 19:01:26 +0100

    prueba para cumpleaños

--- src/main/java/es/iesjandula/reaktor/notifications_server/utils/Constants.java
+++ src/main/java/es/iesjandula/reaktor/notifications_server/utils/Constants.java
@@ -85,7 +85,7 @@ public class Constants
 	/*********************************************************/
 	
 	/** Crono - Felicitacion */
-	public static final String CRON_FELICITACION = "0 0 0 * * *";
+	public static final String CRON_FELICITACION = "0 3 19 * * *";
 
 
 	/*********************************************************/
