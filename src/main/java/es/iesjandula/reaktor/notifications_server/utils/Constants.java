ce6c28a22bfaa1f888d2d2fed427d2ba8734b593
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2025-11-19 21:12:31 +0100

    actualizar cron

--- src/main/java/es/iesjandula/reaktor/notifications_server/utils/Constants.java
+++ src/main/java/es/iesjandula/reaktor/notifications_server/utils/Constants.java
@@ -85,7 +85,7 @@ public class Constants
 	/*********************************************************/
 	
 	/** Crono - Felicitacion */
-	public static final String CRON_FELICITACION = "30 10 21 * * *";
+	public static final String CRON_FELICITACION = "15 14 21 * * *";
 
 
 	/*********************************************************/
