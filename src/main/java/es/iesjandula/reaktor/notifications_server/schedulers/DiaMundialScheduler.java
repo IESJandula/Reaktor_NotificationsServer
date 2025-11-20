e31355fd978cefd36e8e5bdf3c4a61c1af1738bb
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2025-11-20 19:39:31 +0100

    import no necesario

--- src/main/java/es/iesjandula/reaktor/notifications_server/schedulers/DiaMundialScheduler.java
+++ src/main/java/es/iesjandula/reaktor/notifications_server/schedulers/DiaMundialScheduler.java
@@ -2,7 +2,6 @@ package es.iesjandula.reaktor.notifications_server.schedulers;
 
 import java.time.LocalDateTime;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
